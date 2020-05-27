eaca42dfcec7a93ea1eb33f9eb65cc72e7263e52
Tristan <tristanleroy26@gmail.com>
2020-05-27 18:36:25 +0200

    VERSION 0.0.1, add base for the docstring

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,34 +1,67 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord.ext.commands import Bot as BotBase
 
-PREFIX = "."
-OWNER_IDS = [175990133240233984]
+PREFIX = "." # Prefix constant
+OWNER_IDS = [175990133240233984] # List of owner ids
 
 class Bot(BotBase):
+	"""
+	Summary line
+
+		TODO:
+			* Do the docstring
+    """
 	def __init__(self):
+		"""
+
+        """
 		self.PREFIX = PREFIX
 		self.ready = False
 		# self.guild = None # Single server 
-		self.scheduler = AsyncIOScheduler()
+		self.scheduler = AsyncIOScheduler() #
 
 		super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS)
 
 	def run(self, version):
+		"""
+		Start and run the bot
+	  
+	        Parameters: 
+		        version (string): String to define the version of the bot
+		"""
 		self.VERSION = version
 
 		with open("./lib/bot/token.0", "r", encoding="utf-8") as tf:
-			self.TOKEN = tf.read()
+			self.TOKEN = tf.read() # Read the token from "./lib/bot/token.0"
 
 		print("running bot...")
 		super().run(self.TOKEN, reconnect=True)
 
 	async def on_connect(self):
+		"""
+		Summary line
+
+			TODO:
+				* Do the docstring
+	    """
 		print("Bot connected")
 
 	async def on_disconnect(self):
+		"""
+		Summary line
+
+			TODO:
+				* Do the docstring
+	    """
 		print("Bot disconnected")
 
 	async def on_ready(self):
+		"""
+		Summary line
+
+			TODO:
+				* Do the docstring
+	    """
 		if not self.ready:
 			self.ready = True
 			# self.guild = self.get_guild(549103172275404810) # Single server 
@@ -38,6 +71,12 @@ class Bot(BotBase):
 			print("bot reconnected")
 
 	async def on_message(self, message):
+		"""
+		Summary line
+
+			TODO:
+				* Do the docstring
+	    """
 		pass
 
 
--- lib/db/db.py
+++ lib/db/db.py
@@ -1,14 +1,33 @@
 from os.path import isfile
 from sqlite3 import connect
 
-DB_PATH = "./data/db/database.db"
-BUILD_PATH = "./data/db/build.sql"
+DB_PATH = "./data/db/database.db" #
+BUILD_PATH = "./data/db/build.sql" # 
 
-cxn = connect(DB_PATH, check_same_thread=False)
-cur = cxn.cursor()
+cxn = connect(DB_PATH, check_same_thread=False) # Sqlite3 connection
+cur = cxn.cursor() # Sqlite3 cursor
 
 def with_commit(func):
+	"""
+	Summary line
+  
+        Parameters: 
+            fun ():
+
+        TODO:
+        	* Do the doctring
+	"""
 	def inner(*args, **kwargs):
+		"""
+		Summary line
+	  
+	        Parameters: 
+		        *args: Variable length argument list.
+		        **kwargs: Arbitrary keyword arguments.
+
+	        TODO:
+	        	* Do the doctring
+		"""
 		func(*args, **kwargs)
 		commit()
 
@@ -17,19 +36,41 @@ def with_commit(func):
 
 @with_commit
 def build():
+	"""
+	Build a database with the path var (BUILD_PATH)
+	"""
 	if isfile(BUILD_PATH):
 		scriptexec(BUILD_PATH)
 
 
 def commit():
+	"""
+	Commit the database change
+	"""
 	cxn.commit()
 
 
 def close():
+	"""
+	Close the sqlite3 connection
+	"""
 	cxn.close()
 
 
 def field(command, *values):
+	"""
+	Summary line
+  
+        Parameters: 
+            command ():
+            *values ():
+          
+        Returns: 
+            fetchone()[0]:
+
+        TODO:
+        	* Do the doctring
+	"""
 	cur.execute(command, tuple(values))
 
 	if (fetch := cur.fetchone()) is not None:
@@ -37,29 +78,98 @@ def field(command, *values):
 
 
 def record(command, *values):
+	"""
+	Summary line
+  
+        Parameters: 
+            command ():
+            *values: Variable length values list.
+          
+        Returns: 
+            fetchone():
+
+        TODO:
+        	* Do the doctring
+        
+	"""
 	cur.execute(command, tuple(values))
 
 	return cur.fetchone()
 
 def records(command, *values):
+	"""
+	Summary line
+  
+        Parameters: 
+            command ():
+            *values: Variable length values list.
+          
+        Returns: 
+            fetchall():
+
+        TODO:
+        	* Do the doctring
+        
+	"""
 	cur.execute(command, tuple(values))
 
 	return cur.fetchall()
 
 def column(command, *values):
+	"""
+	Summary line
+  
+        Parameters: 
+            command ():
+            *values: Variable length values list.
+          
+        Returns: 
+
+        TODO:
+        	* Do the doctring
+	"""
 	cur.execute(command, tuple(values))
 
 	return [item[0] for item in cur.fetchall()]
 
 
 def execute(command, *values):
+	"""
+	Summary line 
+  
+        Parameters: 
+            command ():
+            *values: Variable length values list.
+
+        TODO:
+        	* Do the doctring
+	"""
 	cur.execute(command, tuple(values))
 
 
 def multiexec(command, valueset):
+	"""
+	Summary line
+  
+        Parameters: 
+   	        command  ():
+	        valueset ():
+
+        TODO:
+        	* Do the doctring
+	"""
 	cur.executemany(command, valueset)
 
 
 def scriptexec(path):
+	"""
+	Summary line
+  
+        Parameters: 
+	        path ():
+
+        TODO:
+        	* Do the doctring
+	"""
 	with open(path, "r", encoding="utf-8") as script:
 		cur.executescript(script.read())
